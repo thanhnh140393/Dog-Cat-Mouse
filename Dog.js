d6baa0140e15c0fab9f31b719107f0c473c2a512
unknown <thakdao@gmail.com>
2018-09-04 22:26:50 +0900

    .gittignore

--- Dog.js
+++ Dog.js
@@ -1,3 +1,11 @@
 function Dog(name){
 	this.name = Dog;
+	this.stomach = [];
 }
+Dog.prototype.eat = function(cat) {
+	this.stomach.push(cat);
+};
+Dog.prototype.sayHi = function() {
+	console.log('Hi! I am a Dog . My name is ' + chalk.red(this.name));
+};
+module.exports = Dog ;
